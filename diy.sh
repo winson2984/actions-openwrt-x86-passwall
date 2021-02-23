2505973ccb42a04908f74b54d0ae442156e29046
Dre Johnson <dre@phreshr.com>
2021-02-23 09:43:35 +0800

    fixed upx package name

--- diy.sh
+++ diy.sh
@@ -2,6 +2,6 @@
 
 cp -R ../default-settings package/default-settings
 
-git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+git clone https://github.com/kuoruan/openwrt-upx.git package/upx
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
