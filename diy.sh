ca3b720a9ac4a46053a890f5e5066e4acb190d4b
Dre Johnson <dre@phreshr.com>
2021-02-23 09:11:17 +0800

    update shell script

---
+++ diy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -R ../default-settings package/default-settings
+
+git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
