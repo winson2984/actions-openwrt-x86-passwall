3eece2e9ef66e8ce74428893f1a18c03d10c5ba9
Dre Johnson <dre@phreshr.com>
2021-02-26 19:40:05 +0800

    remove openwrt-upx, build luci-app-openclash

--- diy.sh
+++ diy.sh
@@ -2,6 +2,22 @@
 
 cp -R ../default-settings package/default-settings
 
-git clone https://github.com/Hyy2001X/openwrt-upx.git package/upx
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
+cd ../..
+make package/luci-app-openclash/luci-app-openclash/compile V=99
