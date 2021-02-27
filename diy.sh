3af05b37a3d68151dea5976a663ce61f4d9d643f
Dre Johnson <dre@phreshr.com>
2021-02-28 00:03:37 +0800

    remove openclash

--- diy.sh
+++ diy.sh
@@ -4,20 +4,3 @@ cp -R ../default-settings package/default-settings
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
-
-# 编译 po2lmo (如果有po2lmo可跳过)
-pushd luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
-
-cd ../..
-make package/luci-app-openclash/luci-app-openclash/compile V=99
